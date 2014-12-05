a791f2196b88d5c417ddbdf216d36361d9b81c91
Jerome Leclanche <jerome@leclan.ch>
2014-12-05 05:01:21 +0100

    NNE: implement on() decorator

--- fireplace/carddata/card.py
+++ fireplace/carddata/card.py
@@ -1,3 +1,12 @@
 from ..targeting import *
 from ..xmlcard import XMLCard as Card
 from .helpers import *
+
+
+def on(event):
+	def decorator(func):
+		func.event = event
+		def wrapper(*args, **kwargs):
+			func(*args, **kwargs)
+		return wrapper
+	return decorator
