ca860d07a89ba7f051d89b43f371e09a0dd8939d
Jerome Leclanche <jerome@leclan.ch>
2014-12-05 05:35:57 +0100

    NNE: Bind event listeners to card instances

--- fireplace/carddata/card.py
+++ fireplace/carddata/card.py
@@ -1,12 +1,24 @@
 from ..targeting import *
-from ..xmlcard import XMLCard as Card
+from ..enums import Zone
+from ..xmlcard import XMLCard
 from .helpers import *
 
 
 def on(event):
 	def decorator(func):
-		func.event = event
 		def wrapper(*args, **kwargs):
 			func(*args, **kwargs)
+		wrapper.event = event
 		return wrapper
 	return decorator
+
+
+class Card(XMLCard):
+	def __new__(cls, id):
+		instance = super().__new__(cls)
+		cls._eventListeners = {}
+		for name, func in cls.__dict__.items():
+			if name == "inPlay":
+				# TODO multiple defs for same zone
+				cls._eventListeners[Zone.PLAY] = {func.event: func}
+		return instance
