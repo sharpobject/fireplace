eb8a90a5083c94d1a0b563ca8431805d28ad5508
Jerome Leclanche <jerome@leclan.ch>
2014-12-16 07:36:27 +0000

    WIP: Warrior

--- fireplace/cards/classic/warrior.py
+++ fireplace/cards/classic/warrior.py
@@ -1,41 +1,69 @@
-import random
-from fireplace.enums import Zone
-from ..card import *
+##
+# Minions
 
+# Armorsmith
+class EX1_402:
+	def OWN_DAMAGE(self, source, target, amount):
+		if target.type == CardType.MINION:
+			self.controller.hero.armor += 1
+
+
+# Cruel Taskmaster
+class EX1_603:
+	def action(self, target):
+		target.buff("EX1_603e")
+		self.hit(target, 1)
+
+class EX1_603e:
+	Atk = 2
+
+
+# Frothing Berserker
+class EX1_604:
+	def DAMAGE(self, source, target, amount):
+		if target.type == CardType.MINION:
+			self.buff("EX1_604o")
+
+class EX1_604o:
+	Atk = 1
+
+
+##
+# Spells
 
 # Charge
-class CS2_103(Card):
+class CS2_103:
 	action = buffTarget("CS2_103e2")
 
-class CS2_103e2(Card):
+class CS2_103e2:
 	Atk = 2
 	Charge = True
 
 
 # Rampage
-class CS2_104(Card):
+class CS2_104:
 	action = buffTarget("CS2_104e")
 
-class CS2_104e(Card):
+class CS2_104e:
 	Atk = 3
 	Health = 3
 
 
 # Heroic Strike
-class CS2_105(Card):
+class CS2_105:
 	action = buffSelf("CS2_105e")
 
-class CS2_105e(Card):
+class CS2_105e:
 	Atk = 4
 
 
 # Execute
-class CS2_108(Card):
+class CS2_108:
 	action = destroyTarget
 
 
 # Cleave
-class CS2_114(Card):
+class CS2_114:
 	def action(self):
 		targets = random.sample(self.controller.opponent.field, 2)
 		for target in targets:
@@ -43,7 +71,7 @@ class CS2_114(Card):
 
 
 # Slam
-class EX1_391(Card):
+class EX1_391:
 	def action(self, target):
 		self.hit(target, 2)
 		if target.zone == Zone.PLAY:
@@ -51,7 +79,7 @@ class EX1_391(Card):
 
 
 # Battle Rage
-class EX1_392(Card):
+class EX1_392:
 	def action(self):
 		for target in self.controller.getTargets(TARGET_FRIENDLY_CHARACTERS):
 			if target.damage:
@@ -59,14 +87,14 @@ class EX1_392(Card):
 
 
 # Whirlwind
-class EX1_400(Card):
+class EX1_400:
 	def action(self):
 		for target in self.controller.getTargets(TARGET_ALL_MINIONS):
 			self.hit(target, 1)
 
 
 # Brawl
-class EX1_407(Card):
+class EX1_407:
 	def action(self):
 		board = self.controller.getTargets(TARGET_ALL_MINIONS)
 		for minion in random.sample(board, len(board) - 1):
@@ -74,13 +102,13 @@ class EX1_407(Card):
 
 
 # Mortal Strike
-class EX1_408(Card):
+class EX1_408:
 	def action(self, target):
 		self.hit(target, 6 if self.controller.hero.health <= 12 else 4)
 
 
 # Upgrade!
-class EX1_409(Card):
+class EX1_409:
 	def action(self):
 		if self.controller.hero.weapon:
 			self.controller.hero.weapon.buff("EX1_409e")
@@ -88,28 +116,28 @@ class EX1_409(Card):
 		else:
 			self.controller.summon("EX1_409t")
 
-class EX1_409e(Card):
+class EX1_409e:
 	Atk = 1
 
 
 # Shield Slam
-class EX1_410(Card):
+class EX1_410:
 	def action(self, target):
 		self.hit(target, self.controller.hero.armor)
 
 
 # Shield Block
-class EX1_606(Card):
+class EX1_606:
 	def action(self):
 		self.controller.hero.armor += 5
 		self.controller.draw()
 
 
 # Inner Rage
-class EX1_607(Card):
+class EX1_607:
 	def action(self, target):
 		target.buff("EX1_607e")
 		self.hit(target, 1)
 
-class EX1_607e(Card):
+class EX1_607e:
 	Atk = 2
